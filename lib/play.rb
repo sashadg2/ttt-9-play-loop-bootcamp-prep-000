1bd9c1e1201b27af5f6117de9db5d8d71c1af382
Oleksandr Diuriagin <sashadg@gmail.com>
2018-02-22 02:07:49 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -12,11 +12,11 @@ def input_to_index(user_input)
 end
 
 def move(board, index, current_player = "X")
-  board[index] = current_player
   if current_player == "X"
     current_player == "O"
-  else current_player == "X"
+    else current_player == "X"
   end
+  board[index] = current_player
 end
 
 def position_taken?(board, location)
